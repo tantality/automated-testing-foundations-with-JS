a226ff032d69962db9a9e865e185778e445b68c4
tantality <angel.tretyak@mail.ru>
2022-01-18 02:12:35 +0300

    feat: add multiplication of values of various data types

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ let arr=[["23",true],["82",123],["23","1948"],[23,true],[82,123],[true,true],[nu
 
 let addition=(a,b)=>a+b;
 let subtraction=(a,b)=>a-b;
+let multiplication=(a,b)=>a*b;
 
 function printResults(func){
     arr.forEach((el,index)=>
-    console.log(`${index+1}) (${el[0]} |${typeof el[0]}|, ${el[1]} |${typeof el[1]}|) = `+func(...el))
-    );
+    console.log(`${index+1}) (${el[0]} |${typeof el[0]}|, ${el[1]} |${typeof el[1]}|) = `+func(...el)));
 }
 
 console.log('\n'+'Addition:');
@@ -14,5 +14,8 @@ printResults(addition);
 
 console.log('\n'+'Subtraction:');
 printResults(subtraction);
+
+console.log('\n'+'Multiplication:');
+printResults(multiplication);
 
 
