e1453752a86d6966e217df0400842bb2fad6ea48
tantality <angel.tretyak@mail.ru>
2022-01-18 01:35:04 +0300

    feat: append addition of values of various data types

---
+++ script.js
@@ -0,0 +1,12 @@
+let arr=[["23",true],["82",123],["23","1948"],[23,true],[82,123],[true,true],[null,true],[undefined,true],[undefined,"hello"]];
+
+let addition=(a,b)=>a+b;
+
+function printResults(func){
+    arr.forEach(el=>console.log(func(...el)));
+}
+
+console.log('\n'+'Addition:');
+printResults(addition);
+
+
