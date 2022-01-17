39521301c95392385f69c7377fc9776216a37a18
tantality <angel.tretyak@mail.ru>
2022-01-18 01:40:35 +0300

    feat: add subtraction of values of various data types

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let arr=[["23",true],["82",123],["23","1948"],[23,true],[82,123],[true,true],[null,true],[undefined,true],[undefined,"hello"]];
 
 let addition=(a,b)=>a+b;
+let subtraction=(a,b)=>a-b;
 
 function printResults(func){
     arr.forEach(el=>console.log(func(...el)));
@@ -8,5 +9,8 @@ function printResults(func){
 
 console.log('\n'+'Addition:');
 printResults(addition);
+
+console.log('\n'+'Subtraction:');
+printResults(subtraction);
 
 
